c9d17cf6043e39a83d091448f345bfead8ad2dfa
sgarzon <sgarzon@venturihnos.local>
2021-11-11 17:30:28 -0300

    Nuevos datos

--- venturi-hnos/src/app/app.component.ts
+++ venturi-hnos/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'venturi-hnos';
+  title = 'El sitio de Venturi Hnos';
+  saludo = 'Saludo desde Angular';
 }
